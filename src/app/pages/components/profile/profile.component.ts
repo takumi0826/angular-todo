188372013c0a7a32b4c5888ba8c48911bc26af26
takumi0826 <takumi.s8.26@gmail.com>
2022-03-27 22:23:52 +0900

    userName 取得

--- src/app/pages/components/profile/profile.component.ts
+++ src/app/pages/components/profile/profile.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent{
-  constructor() { }
+export class ProfileComponent implements OnInit{
+  constructor(private auth:AuthService) { }
+  username!: string;
+
+  ngOnInit(): void {
+    this.auth.fetchUser().subscribe(res => {
+      this.username = res.userName;
+    });
+  }
+
 }
